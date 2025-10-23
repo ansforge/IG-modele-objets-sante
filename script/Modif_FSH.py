5ce7acb9dab5c462c7516917244db8ea1a28b24a
paulineheurtebise <122790741+paulineheurtebise@users.noreply.github.com>
2025-10-23 14:46:54 +0200

    Maj script modif FSH

--- script/Modif_FSH.py
+++ script/Modif_FSH.py
@@ -1,26 +1,33 @@
-alias_line = "Alias: $tre-r394-competence-metier = https://smt.esante.gouv.fr/fhir/CodeSystem/tre-r394-competence-metier"
-file_path = "../input/fsh/aliases.fsh"
+################# AJOUT DE LIGNE DANS UN FICHIER FSH
+def add_line_fsh(file_path, line):
+    """
+    Ajoute line dans le fichier file_path s'il n'est pas déjà présent.
+    """
+    try:
+        with open(file_path, "r+", encoding="utf-8") as f:
+            content = f.read()
+            if line.strip() not in content:
+                if not content.endswith("\n"):
+                    f.write("\n")
+                f.write(line)
+    except FileNotFoundError:
+        print(f"❌ Le fichier {file_path} n'existe pas.")
 
-# Lecture du fichier
-with open(file_path, "r+", encoding="utf-8") as f:
-    content = f.read()
-    # On vérifie si la ligne est déjà là
-    if alias_line.strip() not in content:
-        # On se place à la fin et on ajoute la ligne
-        if not content.endswith("\n"):
-            f.write("\n")
-        f.write(alias_line)
+################# ALIAS
 
+TRE_R394 = "Alias: $tre-r394-competence-metier = https://smt.esante.gouv.fr/fhir/CodeSystem/tre-r394-competence-metier"
+aliases_path = "../input/fsh/aliases.fsh"
+add_line_fsh(aliases_path, TRE_R394)
+
+
+################# PROFESSIONNEL
 
 exercicePro_path = "../input/fsh/logicals/Professionnel/ExerciceProfessionnel.fsh"
 ref1 = '* professionnel 1..1 Reference(Professionnel) "Lien vers la classe Professionnel" '
+add_line_fsh(exercicePro_path, ref1)
 
-# Lecture du fichier
-with open(exercicePro_path, "r+", encoding="utf-8") as f:
-    content = f.read()
-    # On vérifie si la ligne est déjà là
-    if ref1.strip() not in content:
-        # On se place à la fin et on ajoute la ligne
-        if not content.endswith("\n"):
-            f.write("\n")
-        f.write(ref1)
+################# STRUCTURE
+
+EG_path = "../input/fsh/logicals/Structure/EntiteGeographique.fsh"
+ref_EG_1 = '* entiteJuridique 1..1 Reference(EntiteJuridique) "Lien vers la classe EntiteJuridique."'
+add_line_fsh(EG_path, ref_EG_1)
